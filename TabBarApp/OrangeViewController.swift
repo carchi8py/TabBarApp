c9a2817b3f89fd480a4e6f6793c5b37094b8cfab
Chris Archibald <carchi8py@gmail.com>
2015-10-12 20:57:28 -0700

    Added source for our 3 tab views.

---
+++ TabBarApp/OrangeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  OrangeViewController.swift
+//  TabBarApp
+//
+//  Created by Chris Archibald on 10/12/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("Orange.didLoad")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("Orange.Will appear")
+        super.viewWillAppear(animated)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
